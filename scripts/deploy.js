c4d36c6f8a1e6aeb084877ca646de97566695cb4
Bryan Campbell <bryanjcampbell1@gmail.com>
2023-11-17 09:14:51 -0500

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const strategyManager = "0xAbc..123";
+  const idealDebtToCollateral = 123;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const vault = await ethers.deployContract("LeveragedWstEth");
+  await vault.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const strategy = await ethers.deployContract("AaveStrategy", [
+    vault.target,
+    strategyManager,
+    idealDebtToCollateral,
+  ]);
 
-  await lock.waitForDeployment();
+  await strategy.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await (await vault.setStrategy(strategy.target)).wait();
+
+  console.log("vault: ", vault.target);
+  console.log("strategy: ", strategy.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
